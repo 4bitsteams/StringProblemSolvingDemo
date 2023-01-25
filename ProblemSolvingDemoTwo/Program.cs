c9b8e1c82fb32e58206de9b1a78be9837c7521c2
md.rubel <md.rubel@HOAP2406101764A>
2023-01-25 18:16:39 +0600

    CheckPalindrome

--- ProblemSolvingDemoTwo/Program.cs
+++ ProblemSolvingDemoTwo/Program.cs
@@ -52,8 +52,59 @@ namespace ProblemSolvingDemoTwo
             if (String.IsNullOrEmpty(inputString))
                 return string.Empty;
 
-            for (int i = inputString.Length - 1; i > 0; i--)
+            for (int i = inputString.Length - 1; i >= 0; i--)
                 result += inputString[i];
+
+            return result;
+        }
+
+        public static string ReverseEachString(string inputString)
+        {
+            string result = "";
+
+            if (string.IsNullOrEmpty(inputString))
+                return string.Empty;
+
+            string[] array = inputString.Split(" ");
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                result += ReverseString(array[i]) + " ";
+            }
+
+            return result;
+        }
+
+        public static int GetWorldCount(string inputString)
+        {
+            if (string.IsNullOrEmpty(inputString))
+                return 0;
+            return inputString.Split(' ').Length;
+        }
+
+
+        public static bool CheckPalindrome(string inputSting)
+        {
+            bool result = true;
+
+            if (string.IsNullOrEmpty(inputSting))
+                return false;
+
+            int min = 0;
+            int max = inputSting.Length - 1;
+
+            while (min < max)
+            {
+                if (inputSting[min] == inputSting[max])
+                {
+                    max--;
+                    min++;
+                }
+                else
+                {
+                    return false;
+                }
+            }
 
             return result;
         }
@@ -64,6 +115,9 @@ namespace ProblemSolvingDemoTwo
             Console.WriteLine(FindDublicate("aaabbbcccddd"));
             Console.WriteLine(FindUnique("aaabbbcccddd"));
             Console.WriteLine(ReverseString("aaabbbcccddd"));
+            Console.WriteLine(ReverseEachString("This Is My Name"));
+            Console.WriteLine(GetWorldCount("This Is My Name Md. Rubel"));
+            Console.WriteLine(CheckPalindrome("madam"));
             Console.WriteLine("Hello World!");
         }
     }
